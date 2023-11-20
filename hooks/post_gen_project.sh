616ec49420053ed01c62c496fc7e00ab641420e0
n-wbrown <n-wbrown@users.noreply.github.com>
2023-11-19 18:50:56 -0800

    init git

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-{% if cookiecutter.auto_git_setup == "yes" %}
 git init
+{% if cookiecutter.auto_git_setup == "yes" %}
 git add -A
 git remote add {{ cookiecutter.git_remote_name }} git@github.com:{{ cookiecutter.github_repo_group }}/{{ cookiecutter.repo_name }}.git
 git commit -am "Initial commit from cookiecutter-pcds-python"

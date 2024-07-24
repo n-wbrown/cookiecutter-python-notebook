2e0f8b25b6043b020f5e4a95c5cc92946ccc64e0
n-wbrown <n-wbrown@users.noreply.github.com>
2024-07-24 12:46:18 -0700

    add option to strip ipynb output

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 git init
+{% if cookiecutter.strip_ipynb_outputs == "yes" %}
+git config filter.strip-notebook-output.clean 'jupyter nbconvert --ClearOutputPreprocessor.enabled=True --to=notebook --stdin --stdout --log-level=ERROR'
+{% endif %}
 {% if cookiecutter.auto_git_setup == "yes" %}
 git add -A
 git remote add {{ cookiecutter.git_remote_name }} git@github.com:{{ cookiecutter.github_repo_group }}/{{ cookiecutter.repo_name }}.git
